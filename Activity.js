1670fc3e1bd498e8fc3a7ca136448709c38b5bf6
Paige Vannelli <paigevannelli@gmail.com>
2021-01-06 14:53:25 -0700

    Add activity class, finish interation 1

---
+++ Activity.js
@@ -0,0 +1,23 @@
+class Activity {
+  constructor() {
+    this.category = null;
+    this.description = null;
+    this.minutes = null;
+    this.seconds = null;
+    this.completed = null;
+    this.id = null;
+  }
+
+  startTimer() {
+
+  }
+
+  markComplete() {
+
+  }
+
+  saveToStorage() {
+
+  }
+  
+}
