15e608b3e018577b3f33ba19f2cbd76c4ff6a099
Matt McVey <matt.mcvey49@gmail.com>
2021-01-06 20:06:05 -0700

    Started functionality for handling minutes and seconds and variable e in input

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ var buttonRow = document.getElementById("buttonRow")
 buttonRow.addEventListener("click", function(event){
   event.preventDefault();
 })
+
+//target minutes and seconds input
+// add addEventListener start activity button
+//
+
+var minutes = document.getElementById("minutes");
+var seconds = document.getElementById("seconds");
+var startActivityButton = document.getElementById("startActivityButton");
+
+startActivityButton.addEventListener("click", handleTime);
+
+function handleTime(event) {
+  event.preventDefault(event);
+  minutes.value
+  if(minutes.value.includes("e")){
+    
+  }
+}
